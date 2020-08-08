9af508b90360836373e5a27487fda017275d94ec
Giovanni Wu <GioVan703@Gios-Big-Mac.cable.rcn.com>
2020-08-08 19:54:25 -0400

    Adding Pyroll.py file.

---
+++ PyPoll.py
@@ -0,0 +1,25 @@
+# The data we need to retrieve.
+# 1. The total number of votes cast
+# 2. A complete list of candidates who received votes
+# 3. The percentage of votes each candidate won
+# 4. The total number of votes each candidate won
+# 5. The winner of the election based on popular vote.
+# Add our dependencies.
+import csv
+import os
+# Assign a variable to load a file from a path.
+file_to_load = os.path.join("Resources", "election_results.csv")
+
+# Open the election results and read the file.
+with open(file_to_load) as election_data:
+     print(election_data)
+# Create a filename variable to a direct or indirect path to the file.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# Using the with statement open the file as a text file.
+outfile = open(file_to_save, "w")
+# Write some data to the file.
+outfile.write("Hello World")
+
+# Close the file
+outfile.close()
